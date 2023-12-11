ca1e24a16748d18a29ba79d216a3a93d893aabc3
Alexandre <alexandrenicoud76@gmail.com>
2023-12-11 16:56:38 +0100

    testing database

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -2,3 +2,30 @@ create table item (
   id int unsigned primary key auto_increment not null,
   title varchar(255) not null
 );
+
+
+create table Profil(
+  id int primary key auto_increment not null,
+  name varchar(50) not null,
+  email varchar(50) not null,
+  naissance DATE NOT NULL,
+  civility BOOLEAN NOT NULL,
+  password varchar(50) not null,
+  is admin bool not null,
+)
+
+create table film(
+  id int primary key auto_increment not null,
+  miniature VARCHAR(255) not null,
+  title VARCHAR(255) not null,
+  duration INT not null,
+  release DATE NOT NULL,
+  description VARCHAR(255) not null,
+  is available BOOLEAN NOT NULL,
+)
+
+CREATE TABLE categorie(
+  id int primary key auto_increment not null,
+  name VARCHAR(255) not null,
+  position INT,
+)
