c59a829ab9b9e5b92d1385110e82b206364c6f19
Ange230700 <kouakouangeericstephane@gmail.com>
2023-12-11 17:26:02 +0100

    front dev home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,62 @@
-import Counter from "./components/Counter";
-import logo from "./assets/logo.svg";
-
-import "./App.css";
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React !</p>
-
-        <Counter />
-
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="home">
+      <div className="movies-display-section">
+        <div className="logo-container">
+          <img
+            className="logo"
+            src="https://via.placeholder.com/75x45"
+            alt="wildtube logo"
+          />
+        </div>
+        <div className="dynamic-hero-slider-container">
+          <img
+            src="https://via.placeholder.com/256x140"
+            alt="hero movie slide"
+            className="hero-movie-slide"
+          />
+          <img
+            src="https://via.placeholder.com/256x140"
+            alt="hero movie slide"
+            className="hero-movie-slide"
+          />
+          <img
+            src="https://via.placeholder.com/256x140"
+            alt="hero movie slide"
+            className="hero-movie-slide"
+          />
+          <img
+            src="https://via.placeholder.com/256x140"
+            alt="hero movie slide"
+            className="hero-movie-slide"
+          />
+          <img
+            src="https://via.placeholder.com/256x140"
+            alt="hero movie slide"
+            className="hero-movie-slide"
+          />
+        </div>
+        <ul className="movie-genre-tabs-container">
+          <li className="movie-genre-tab-container">
+            <p className="movie-genre">Toutes les catégories</p>
+          </li>
+          <li className="movie-genre-tab-container">
+            <p className="movie-genre">Action</p>
+          </li>
+          <li className="movie-genre-tab-container">
+            <p className="movie-genre">Aventure</p>
+          </li>
+          <li className="movie-genre-tab-container">
+            <p className="movie-genre">Comédie</p>
+          </li>
+          <li className="movie-genre-tab-container">
+            <p className="movie-genre">Romance</p>
+          </li>
+          <li className="movie-genre-tab-container">
+            <p className="movie-genre">Science-fiction</p>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
