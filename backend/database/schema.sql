562d9fb56fb94ac4db04425aa40ae877cb693482
Alexandre <alexandrenicoud76@gmail.com>
2023-12-11 16:58:23 +0100

    pushing 3 more tables

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,6 +1,6 @@
 create table serie (
   id int unsigned primary key auto_increment not null,
-  miniature varchar(100) not null,
+  miniature varchar(255) not null,
   title VARCHAR(50) not NULL,
   duration INT not NULL,
   release DATE not NULL,
@@ -27,7 +27,7 @@ create table film(
   title VARCHAR(255) not null,
   duration INT not null,
   release DATE NOT NULL,
-  description VARCHAR(255) not null,
+  description VARCHAR(500) not null,
   is available BOOLEAN NOT NULL,
 )
 
