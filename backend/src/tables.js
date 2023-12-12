49912df165a1eb1e8b27ce89ae9637693547ba7d
Techer Tony <techer307@gmail.com>
2023-12-12 15:50:32 +0100

    SerieManager

--- backend/src/tables.js
+++ backend/src/tables.js
@@ -4,9 +4,11 @@
 
 // Import the manager modules responsible for handling data operations on the tables
 const ItemManager = require("./models/ItemManager");
+const SerieManager = require("./models/SerieManager");
 
 const managers = [
   ItemManager,
+  SerieManager,
   // Add other managers here
 ];
 
