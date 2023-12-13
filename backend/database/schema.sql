24146bc695dfeb6af74b71a611e5f6c3d71cdb90
Ange230700 <kouakouangeericstephane@gmail.com>
2023-12-13 10:54:29 +0100

    pending...

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -2,29 +2,29 @@ DROP TABLE IF EXISTS `Serie`;
 
 CREATE TABLE
     `Serie` (
-        `id` int primary key auto_increment not null,
-        `miniature` varchar(255) not null,
-        `title` VARCHAR(50) not NULL,
-        `video_url` VARCHAR(255) not null
-        `duration` INT not NULL,
-        `year` DATE not NULL,
-        `description` VARCHAR(500) not NULL,
-        `is_available` BOOLEAN not NULL,
-        `episodes_number` INT not NULL,
-        `seasons_number` INT not NULL
+        `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        `miniature` VARCHAR(255) NOT NULL,
+        `title` VARCHAR(50) NOT NULL,
+        `video_url` VARCHAR(255) NOT null,
+        `duration` INT NOT NULL,
+        `year` DATE NOT NULL,
+        `description` VARCHAR(500) NOT NULL,
+        `is_available` BOOLEAN NOT NULL,
+        `episodes_number` INT NOT NULL,
+        `seasons_number` INT NOT NULL
     );
 
 DROP TABLE IF EXISTS `User`;
 
 CREATE TABLE
     `User` (
-        `id` int primary key auto_increment not null,
-        `name` varchar(50) not null,
-        `email` varchar(50) not null,
+        `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        `name` VARCHAR(50) NOT NULL,
+        `email` VARCHAR(50) NOT NULL,
         `naissance` DATE NOT NULL,
-        `civility` BOOLEAN NOT NULL,
-        `password` varchar(50) not null,
-        `IsAdmin` bool not null
+        `civility` TINYINT(1) NOT NULL,
+        `password` VARCHAR(50) NOT NULL,
+        `is_admin` TINYINT(1) not null
     );
 
 DROP TABLE IF EXISTS `Film`;
@@ -137,63 +137,3 @@ CREATE TABLE
         CONSTRAINT FK_Categorie_Par_Film_categorie_id FOREIGN KEY (`categorie_id`) REFERENCES `Categorie`(`id`),
         PRIMARY KEY (`film_id`, `categorie_id`)
     );
-
-INSERT INTO 
-    `Film` (`miniature`, `title`, `duration`, `year`, `description`, `is_available`)
-VALUES
-    (
-        'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg',
-        'Avengers: Endgame',
-        181,
-        '2019-04-24',
-        'After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos actions and restore balance to the universe.',
-        1
-    ),
-    (
-        'https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_.jpg',
-        'Avengers: Infinity War',
-        149,
-        '2018-04-25',
-        'The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.',
-        0
-    ),
-    (
-        'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg',
-        'The Avengers',
-        143,
-        '2012-04-25',
-        'Earth mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.',
-        1
-    ),
-    (
-        'https://static.wikia.nocookie.net/ironman/images/d/da/P170620_v_v8_ba.jpg/revision/latest?cb=20191202183622',
-        'Iron man',
-        126,
-        '2008-04-30',
-        'After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.',
-        0
-    );
-
-INSERT INTO
-    `Categorie` (`name`, `position`)
-VALUES
-    ('Action', 1),
-    ('Adventure', 2),
-    ('Sci-Fi', 3),
-    ('Drama', 4),
-    ('Thriller', 5),
-    ('Comedy', 6),
-    ('Crime', 7),
-    ('Fantasy', 8),
-    ('Mystery', 9),
-    ('Animation', 10),
-    ('Family', 11),
-    ('Biography', 12),
-    ('History', 13),
-    ('Horror', 14),
-    ('Music', 15),
-    ('Musical', 16),
-    ('Romance', 17),
-    ('Sport', 18),
-    ('War', 19),
-    ('Western', 20);
