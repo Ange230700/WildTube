358f9e1fc98d38c077c8af4189e62eb383002801
Ange230700 <kouakouangeericstephane@gmail.com>
2023-12-13 16:00:36 +0100

    creating react routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
 import "./sass/index.scss";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "search",
+        element: <Search />,
+      },
+      // {
+      //   path: "profile",
+      //   element: <h1>Profile</h1>,
+      // },
+    ],
   },
 ]);
 
