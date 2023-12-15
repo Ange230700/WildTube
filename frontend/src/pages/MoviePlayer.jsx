e166c85d4eb5db61585ee6365036c0dda46f10ce
Ange230700 <kouakouangeericstephane@gmail.com>
2023-12-15 10:43:26 +0100

    pending...

--- frontend/src/pages/MoviePlayer.jsx
+++ frontend/src/pages/MoviePlayer.jsx
@@ -1,30 +1,43 @@
-/* eslint-disable jsx-a11y/media-has-caption */
-import { useParams } from "react-router-dom";
+/* eslint-disable react/self-closing-comp  */
+import { useParams, NavLink } from "react-router-dom";
 import { useMovies } from "../contexts/MovieContext";
 
 function MoviePlayer() {
   const { movieId } = useParams();
   const { movies } = useMovies();
   return (
-    <div className="movie-player-wrapper">
-      {movies
-        .filter((movie) => {
-          return movie.id === parseInt(movieId, 10);
-        })
-        .map((movie) => {
-          //   console.info(movie.videoUrl);
-          return (
-            <div className="movie-player-container" key={movie.id}>
-              <div className="movie-player">
-                <video
-                  className="movie-player-video"
-                  src={movie.videoUrl}
-                  controls
-                />
-              </div>
-            </div>
-          );
-        })}
+    <div className="movie-player">
+      <div className="back-to-previous-page">
+        <NavLink to={`/movies/${movieId}`}>
+          <img
+            className="back-arrow"
+            src="/src/assets/icons/circle-arrow-left-solid.svg"
+            alt="back arrow"
+          />
+        </NavLink>
+      </div>
+      <div className="movie-player-wrapper">
+        {movies
+          .filter((movie) => {
+            return movie.id === parseInt(movieId, 10);
+          })
+          .map((movie) => {
+            const videoId = new URL(movie.videoUrl).searchParams.get("v");
+            const embedUrl = `https://www.youtube.com/embed/${videoId}`;
+            return (
+              <iframe
+                key={movie.id}
+                width="789"
+                height="328"
+                src={embedUrl}
+                title="Marvel Studios&#39; Avengers: Infinity War Official Trailer"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+              ></iframe>
+            );
+          })}
+      </div>
     </div>
   );
 }
